b0ecf21ef6c739f1b2dfffd509e5e9e73fa79cff
Diego Garces <thebroyt.290@gmail.com>
2024-03-11 13:32:00 -0600

    Add Button Report's

--- js/Hipervinculos.js
+++ js/Hipervinculos.js
@@ -1,9 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.ir');
     const volverButtons = document.querySelectorAll('.volver');
 
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function() {
+    const verPdfContainer = document.getElementById('verPdf');
+    const pdfToast = new bootstrap.Toast(document.getElementById('pdfToast'));
+
+    const verPdfGeneralContainer = document.getElementById('verPdfGeneral');
+    const pdfToastGeneral = new bootstrap.Toast(document.getElementById('pdfToastGeneral'));
+
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
             const producto = this.getAttribute('data-producto');
             const urlDestino = `${producto}.html`;
 
@@ -12,10 +18,26 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    volverButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
+    volverButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
             // Redirige al usuario a la página index.html
             window.location.href = 'index.html';
         });
+    });
+    verPdfContainer.addEventListener('click', function() {
+        pdfToast._element.classList.remove('d-none');
+        pdfToast.show();
+    });
+    pdfToast._element.addEventListener('hidden.bs.toast', function () {
+        pdfToast._element.classList.add('d-none');
+    });
+
+    verPdfGeneralContainer.addEventListener('click', function () {
+        pdfToastGeneral._element.classList.remove('d-none');
+        pdfToastGeneral.show();
+    });
+
+    pdfToastGeneral._element.addEventListener('hidden.bs.toast', function () {
+        pdfToastGeneral._element.classList.add('d-none');
     });
 });
